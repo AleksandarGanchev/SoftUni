70563aebbfdd330a53fbee39d09497663993153a
Aleksandar Ganchev <ganchev.aleksandar@yahoo.com>
2023-12-05 12:12:16 +0000

    added solution

---
+++ Programming Basics Python/First Steps in Coding - Lab/07. Projects Creation.py
@@ -0,0 +1,5 @@
+architect_name = input()
+projects_count = int(input())
+project_time = 3
+
+print(f'The architect {architect_name} will need {projects_count * project_time} hours to complete {projects_count} project/s.')
---
+++ Programming Basics Python/First Steps in Coding - Lab/08. Pet Shop.py
@@ -0,0 +1,7 @@
+count_food_dogs = int(input())
+count_food_cats = int(input())
+dog_price = 2.50
+cat_price = 4
+
+total = (count_food_dogs * dog_price) + (count_food_cats * cat_price)
+print(f'{total} lv.')
