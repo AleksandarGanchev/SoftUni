2d631843009dbe7de15be26d8d287c4bce1a1afd
Aleksandar Ganchev <144393141+AleksandarGanchev@users.noreply.github.com>
2023-12-11 23:26:02 +0000

    added solution

---
+++ Fundamentals Python/Data Types and Variables - Exercise/01. Integer Operations.py
@@ -0,0 +1,10 @@
+first_number = int(input())
+second_number = int(input())
+third_number = int(input())
+forth_number = int(input())
+
+sum_two= (first_number+second_number)
+divide = sum_two / third_number
+multiply = int(divide) * forth_number
+
+print(multiply)
