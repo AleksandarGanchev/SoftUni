e22f5892f39b71c6dfde52ae3596706dcec935eb
Aleksandar Ganchev <ganchev.aleksandar@yahoo.com>
2023-12-31 23:41:11 +0000

    added solution

--- Python Advanced/Lists as Stacks and Queues - Exercise/6_balanced_parentheses.py
+++ Python Advanced/Lists as Stacks and Queues - Exercise/6_balanced_parentheses.py
@@ -1,29 +1,58 @@
 from collections import deque
 
-stack = deque([])
-parentheses = input()
+parentheses = deque(input())
+open_parentheses = deque()
 
-all_matched = True
-for element in parentheses:
-    if element == "{" or element == "[" or element == "(":
-        stack.append(element)
+while parentheses:
+    current_parentheses = parentheses.popleft()
 
-    elif "{" in stack or "[" in stack or "(" in stack:
-        if element == "}" and stack[-1] == "{":
-            stack.pop()
-        elif element == "]" and stack[-1] == "[":
-            stack.pop()
-        elif element == ")" and stack[-1] == "(":
-            stack.pop()
-        else:
-            all_matched = False
+    if current_parentheses in "{[(":
+        open_parentheses.append(current_parentheses)
+    elif not open_parentheses:
+        print("NO")
+        break
+    else:
+        if f"{open_parentheses.pop() + current_parentheses}" not in "{}[]()":
             print("NO")
             break
 
-    else:
-        all_matched = False
-        print("NO")
-        break
-
-if all_matched:
+else:
     print("YES")
+
+
+
+
+
+
+
+
+
+# from collections import deque
+#
+# stack = deque([])
+# parentheses = input()
+#
+# all_matched = True
+# for element in parentheses:
+#     if element in "{[(":
+#         stack.append(element)
+#
+#     elif "{[(" in stack:
+#         if element == "}" and stack[-1] == "{":
+#             stack.pop()
+#         elif element == "]" and stack[-1] == "[":
+#             stack.pop()
+#         elif element == ")" and stack[-1] == "(":
+#             stack.pop()
+#         else:
+#             all_matched = False
+#             print("NO")
+#             break
+#
+#     else:
+#         all_matched = False
+#         print("NO")
+#         break
+#
+# if all_matched:
+#     print("YES")
