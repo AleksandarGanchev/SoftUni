0ed66a2ab3b24d0b9b4822e8795fba247347441e
Aleksandar Ganchev <ganchev.aleksandar@yahoo.com>
2024-02-12 20:44:01 +0000

    problem solution

--- Python Fundamentals/Dictionaries - More Exercises/02_judge.py
+++ Python Fundamentals/Dictionaries - More Exercises/02_judge.py
@@ -1,48 +1,31 @@
 command = input()
-judge = {}
-individual_standings = {}
+judge, individual = {}, {}
+
 while command != 'no more time':
-    username, contest, points = command.split(' -> ')
-    points = int(points)
+    username, contest, points = [int(x) if x.isdigit() else x for x in command.split(' -> ')]
 
     if contest not in judge:
         judge[contest] = {username: points}
-    else:
-        if username not in judge[contest]:
-            judge[contest][username] = points
-        else:
-            if points > judge[contest][username]:
-                judge[contest][username] = points
-
-    if username not in individual_standings:
-        individual_standings[username] = {contest: points}
-    else:
-        if contest in individual_standings[username]:
-            if points > individual_standings[username][contest]:
-                individual_standings[username][contest] = points
-        else:
-            individual_standings[username][contest] = points
+    elif username not in judge[contest]:
+        judge[contest][username] = points
+    elif points > judge[contest][username]:
+        judge[contest][username] = points
     command = input()
 
-for contest_name, participants in judge.items():
-    print(f"{contest_name}: {len(participants)} participants")
-    if len(participants) > 0:
-        sorted_participants = sorted(participants.items(), key=lambda x: (-x[1], x[0]))
-        counter = 1
-        for name, points in sorted_participants:
-            print(f"{counter}. {name} <::> {points}")
-            counter += 1
-
-new_dict = {}
-for test_name, course in individual_standings.items():
-    for key, value in course.items():
-        if test_name not in new_dict:
-            new_dict[test_name] = value
-        else:
-            new_dict[test_name] += value
-
-output = dict(sorted(new_dict.items(), key=lambda x: (-x[1], x[0])))
+for course, data in judge.items():
+    print(f"{course}: {len(data)} participants")
+    sorted_by_points = dict(sorted(data.items(), key=lambda x: (-x[1], x[0])))
+    for rank, (name, total_points) in enumerate(sorted_by_points.items()):
+        print(f"{rank+1}. {name} <::> {total_points}")
 
 print("Individual standings:")
-for index, (name, points) in enumerate(output.items()):
-    print(f"{index+1}. {name} -> {points}")
+for course, info in judge.items():
+    for name, points in info.items():
+        if name not in individual:
+            individual[name] = points
+        else:
+            individual[name] += points
+
+sorted_by_points = dict(sorted(individual.items(), key=lambda x: (-x[1], x[0])))
+for rank, (username, total_points) in enumerate(sorted_by_points.items()):
+    print(f"{rank+1}. {username} -> {total_points}")
