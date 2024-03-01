199d1b604abd7572c36f3fde0ffc5563e216b724
Aleksandar Ganchev <ganchev.aleksandar@yahoo.com>
2024-03-01 01:44:28 +0000

    problem solution

---
+++ Python Advanced/ Python OOP - Exercises/Inheritance - Exercise/05_shop/project/product_repository.py
@@ -0,0 +1,23 @@
+from project.product import Product
+
+
+class ProductRepository:
+    def __init__(self) -> None:
+        self.products = []
+
+    def add(self, product: Product) -> None:
+        self.products.append(product)
+
+    def find(self, product_name: str):
+        for product in self.products:
+            if product.name == product_name:
+                return product
+
+    def remove(self, product_name: str):
+        product = self.find(product_name)
+        if product:
+            self.products.remove(product)
+
+    def __repr__(self):
+        # print(f"{x.name}: {x.quantity}" for x in self.products)
+        return "\n".join(f"{x.name}: {x.quantity}" for x in self.products)
