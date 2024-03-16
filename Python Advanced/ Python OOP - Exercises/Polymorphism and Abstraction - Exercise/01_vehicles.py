8613cc54855290ac43c3c666a500e5b69c384d55
Aleksandar Ganchev <ganchev.aleksandar@yahoo.com>
2024-03-16 21:29:09 +0000

    problem solution

---
+++ Python Advanced/ Python OOP - Exercises/Polymorphism and Abstraction - Exercise/01_vehicles.py
@@ -0,0 +1,43 @@
+
+from abc import ABC, abstractmethod
+
+
+class Vehicle(ABC):
+    @abstractmethod
+    def drive(self, distance):
+        ...
+
+    @abstractmethod
+    def refuel(self, fuel: int):
+        ...
+
+
+class Car(Vehicle):
+    def __init__(self, fuel_quantity: int, fuel_consumption: int):
+        self.fuel_quantity = fuel_quantity
+        self.fuel_consumption = fuel_consumption
+        self.air_con_consumptions = 0.9
+
+    def drive(self, distance) -> None:
+        consumption = distance * (self.fuel_consumption + self.air_con_consumptions)
+        if self.fuel_quantity >= consumption:
+            self.fuel_quantity -= consumption
+
+    def refuel(self, fuel) -> None:
+        self.fuel_quantity += fuel
+
+
+class Truck(Vehicle):
+    def __init__(self, fuel_quantity: int, fuel_consumption: int):
+        self.fuel_quantity = fuel_quantity
+        self.fuel_consumption = fuel_consumption
+        self.air_con_consumptions = 1.6
+        self.fuel_leak = 0.95
+
+    def drive(self, distance) -> None:
+        consumption = distance * (self.fuel_consumption + self.air_con_consumptions)
+        if self.fuel_quantity >= consumption:
+            self.fuel_quantity -= consumption
+
+    def refuel(self, fuel) -> None:
+        self.fuel_quantity += fuel * self.fuel_leak
