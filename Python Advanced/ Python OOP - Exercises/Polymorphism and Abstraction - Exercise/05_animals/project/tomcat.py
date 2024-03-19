9c6720eb9de62bd4eac47ae5af57fbb7feb9c4d4
Aleksandar Ganchev <ganchev.aleksandar@yahoo.com>
2024-03-19 15:51:17 +0000

    problem solution

---
+++ Python Advanced/ Python OOP - Exercises/Polymorphism and Abstraction - Exercise/05_animals/project/tomcat.py
@@ -0,0 +1,12 @@
+from project.cat import Cat
+
+
+class Tomcat(Cat):
+    def __init__(self, name: str, age: int):
+        super().__init__(name, age, gender="Male")
+
+    def make_sound(self) -> str:
+        return "Hiss"
+
+    def __repr__(self):
+        return f"This is {self.name}. {self.name} is a {self.age} year old {self.gender} {self.__class__.__name__}"
