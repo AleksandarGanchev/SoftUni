49e9170e4bb51e2e5034045e6a66ec8297a15bf7
Aleksandar Ganchev <ganchev.aleksandar@yahoo.com>
2024-03-19 01:06:30 +0000

    problem solution

---
+++ Python Advanced/ Python OOP - Exercises/Polymorphism and Abstraction - Exercise/04_wild_farm/project/animals/birds.py
@@ -0,0 +1,29 @@
+from project.animals.animal import Bird
+from project.food import Meat, Seed, Fruit, Vegetable
+
+
+class Owl(Bird):
+    def __init__(self, name: str, weight: float, wing_size: float):
+        super().__init__(name, weight, wing_size)
+        self.desired_food = [Meat]
+
+    @property
+    def weight_gain(self):
+        return 0.25
+
+    def make_sound(self):
+        return "Hoot Hoot"
+
+
+class Hen(Bird):
+    def __init__(self, name: str, weight: float, wing_size: float):
+        super().__init__(name, weight, wing_size)
+        self.desired_food = [Meat, Vegetable, Fruit, Seed]
+
+    @property
+    def weight_gain(self):
+        return 0.35
+
+    def make_sound(self):
+        return "Cluck"
+
