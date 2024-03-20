7d47d122f74a0d841f903ab14ae3625c51921fe1
Aleksandar Ganchev <ganchev.aleksandar@yahoo.com>
2024-03-20 20:15:23 +0000

    problem solution

--- Python Advanced/ Python OOP - Exercises/Polymorphism and Abstraction - Exercise/05_animals/project/dog.py
+++ Python Advanced/ Python OOP - Exercises/Polymorphism and Abstraction - Exercise/05_animals/project/dog.py
@@ -8,3 +8,4 @@ class Dog(Animal):
 
     def __repr__(self):
         return f"This is {self.name}. {self.name} is a {self.age} year old {self.gender} {self.__class__.__name__}"
+
--- Python Advanced/ Python OOP - Exercises/Polymorphism and Abstraction - Exercise/05_animals/project/tomcat.py
+++ Python Advanced/ Python OOP - Exercises/Polymorphism and Abstraction - Exercise/05_animals/project/tomcat.py
@@ -10,3 +10,4 @@ class Tomcat(Cat):
 
     def __repr__(self):
         return f"This is {self.name}. {self.name} is a {self.age} year old {self.gender} {self.__class__.__name__}"
+
